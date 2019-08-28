57cdc8e88c3c49c0724dd1724810bc80af752f46
Gavin Sykes <gavin@gavinsykes.uk>
2019-08-28 21:58:29 +0100

    Much improved!

--- navbar.php
+++ navbar.php
@@ -2,19 +2,19 @@
 function generateNavbar() {
     $filename = basename($_SERVER['PHP_SELF']);
     echo '<nav>';
-    echo "<a "; if ($filename == "index.php") {echo 'class="active" href="#"';} else {echo 'href="index.php"';} echo ">Home</a>";
-    echo "<a "; if ($filename == "onlinecv.php") {echo 'class="active" href="#"';} else {echo 'href="onlinecv.php"';} echo ">Online CV</a>";
-    echo "<a "; if ($filename == "samples.php") {echo 'class="active" href="#"';} else {echo 'href="samples.php"';} echo ">Samples</a>";
-    echo "<a "; if ($filename == "sandbox.php") {echo 'class="active" href="#"';} else {echo 'href="sandbox.php"';} echo ">Sandbox</a>";
-    echo "<a "; if ($filename == "blog.php") {echo 'class="active" href="#"';} else {echo 'href="blog.php"';} echo ">Blog</a>";
-    echo "<a class=" . '"right email"' . ' id="email"><i class="' . 'fa fa-envelope-o"></i><span> Email</span></a>';
-    echo "<a class=" . '"right twitter"' . ' id="twitter" href="obfuscated" target="_blank"><i class="' . 'fa fa-twitter"></i><span> Twitter</span></a>';
-    echo "<a class=" . '"right stackoverflow"' . ' id="stackoverflow" href="obfuscated" target="_blank"><i class="' . 'fa fa-stack-overflow"></i><span> Stack Overflow</span></a>';
-    echo "<a class=" . '"right linkedin"' . ' id="linkedin" href="obfuscated" target="_blank"><i class="' . 'fa fa-linkedin"></i><span> LinkedIn</span></a>';
-    echo "<a class=" . '"right github"' .  ' id="github" href="obfuscated" target="_blank"><i class="' . 'fa fa-github"></i><span> Github</span></a>';
-    echo '<a class="icon" id="icon" href="javascript:void(0)">&#9776;</a></nav>';
+    echo '<a '; echo $filename == "index.php" ? 'class="active" href="#"' : 'href="index.php"'; echo '>Home</a>';
+    echo '<a '; echo $filename == "page1.php" ? 'class="active" href="#"' : 'href="onlinecv.php"'; echo '>Page 1</a>';
+    echo '<a '; echo $filename == "page2.php" ? 'class="active" href="#"' : 'href="samples.php"'; echo '>Page 2</a>';
+    echo '<a '; echo $filename == "page3.php" ? 'class="active" href="#"' : 'href="sandbox.php"'; echo '>Page 3</a>';
+    echo '<a '; echo $filename == "page4.php" ? 'class="active" href="#"' : 'href="page4.php"'; echo '>page 4</a>';
+    echo '<div class="middle"></div>'
+    echo '<a class="codepen" href="https://codepen.io/gavinsykesuk" target="_blank"><i class="fab fa-codepen"></i><span> Codepen</span></a>';
+    echo '<a class="github" href="https://github.com/gavinsykes" target="_blank"><i class="fab fa-github"></i><span> Github</span></a>';
+    echo '<a class="linkedin" href="https://www.linkedin.com/in/gavinsykes/" target="_blank"><i class="fab fa-linkedin"></i><span> LinkedIn</span></a>';
+    echo '<a class="stackoverflow" href="https://stackoverflow.com/users/8640133/gavin" target="_blank"><i class="fab fa-stack-overflow"></i><span> Stack Overflow</span></a>';
+    echo '<a class="twitter" href="https://www.twitter.com/gavinsykes_uk" target="_blank"><i class="fab fa-twitter"></i><span> Twitter</span></a>';
+    echo '<div id="nav-open" class="open-icon">≡</div>
 }
-?>
 
 // In the page(s) where you want your navbar to appear, insert a `require_once 'navbar.php';` statement and simply call the `generateNavbar()` function where required.
 
